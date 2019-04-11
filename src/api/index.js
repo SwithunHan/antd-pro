72f509716be3cecfc086ab917f36bfbd61a0807a
SwithunHan <18833567817@163.com>
2019-04-11 15:42:24 +0800

    添加首页房源、小区、区域小区数接口

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,27 @@ import {get} from "./fetch"
 
 // const host = "127.0.0.1:8000";
 
-export async function getCompanyOfArea() {
+export async function getComunity(params) {
     try {
-        const res = await get(`/companyofarea`);
+        const res = await get(`/community?${params}`);
+        return await res.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function getIndexHouse(params) {
+    try {
+        const res = await get(`/houselist/?page=1`);
+        return await res.json();
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+export async function getDistribution(params) {
+    try {
+        const res = await get(`/distribution`);
         return await res.json();
     } catch (e) {
         console.log(e);
