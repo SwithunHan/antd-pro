1a58af208755c12057fa4dc2804dcfaf5ea09f8c
SwithunHan <18833567817@163.com>
2019-04-11 15:41:20 +0800

    添加小区信息页面

---
+++ src/views/Community/index.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react'
+import "./style.scss"
+class Community extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <div className="Community"></div>
+        )
+    }
+}
+
+export default Community
